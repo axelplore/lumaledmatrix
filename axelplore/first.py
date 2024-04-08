dfd5f6b7fe293ca8065c1290fae0bfa25481d34e
AD\ASA06 <alexander_sadowski@deichmann.com>
2024-04-08 18:21:56 +0200

    add Befehlszeilenargumente: --cascaded oder -n: Die Anzahl der kaskadierten MAX7219-LED-Matrizen. --block-orientation: Die Ausrichtung der Blöcke, wenn sie vertikal verkabelt sind. --rotate: Die Drehung des Displays. --reverse-order: Eine boolsche Option, um anzugeben, ob die Blöcke in umgekehrter Reihenfolge sind.

--- axelplore/first.py
+++ axelplore/first.py
@@ -2,6 +2,7 @@
 # https://luma-led-matrix.readthedocs.io/en/latest/python-usage.html
 
 import time
+import argparse
 
 from luma.core.interface.serial import spi, noop
 from luma.core.render import canvas
@@ -9,16 +10,35 @@ from luma.led_matrix.device import max7219
 from luma.core.legacy import text
 from luma.core.legacy.font import proportional, CP437_FONT, LCD_FONT
 
-# create device
-serial = spi(port=0, device=0, gpio=noop())
-device = max7219(serial)
-print("Created device")
+def demo(n, block_orientation, rotate, inreverse):
+    # create device
+    serial = spi(port=0, device=0, gpio=noop())
+    device = max7219(serial, cascaded=n or 1, block_orientation=block_orientation,
+                     rotate=rotate or 0, blocks_arranged_in_reverse_order=inreverse)
+    print("Created device")
 
-msg = "Hallo Lisa"
-msg_length= len(msg)
+    msg = "Hallo Lisa"
+    msg_length= len(msg)
 
-for i in range(msg_length):
-    with canvas(device) as draw:
-        text(draw, (0, 0), msg[i], fill="white")
-        print("draw", msg[i])
-        time.sleep(1)
+    for i in range(msg_length):
+        with canvas(device) as draw:
+            text(draw, (0, 0), msg[i], fill="white")
+            print("draw", msg[i])
+            time.sleep(1)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='matrix_demo arguments',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('--cascaded', '-n', type=int, default=1, help='Number of cascaded MAX7219 LED matrices')
+    parser.add_argument('--block-orientation', type=int, default=0, choices=[0, 90, -90], help='Corrects block orientation when wired vertically')
+    parser.add_argument('--rotate', type=int, default=0, choices=[0, 1, 2, 3], help='Rotate display 0=0°, 1=90°, 2=180°, 3=270°')
+    parser.add_argument('--reverse-order', type=bool, default=False, help='Set to true if blocks are in reverse order')
+
+    args = parser.parse_args()
+
+    try:
+        demo(args.cascaded, args.block_orientation, args.rotate, args.reverse_order)
+    except KeyboardInterrupt:
+        pass
